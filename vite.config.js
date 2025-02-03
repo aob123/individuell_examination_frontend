888eda6611b39ec5b91fdac57e8e9e19dab7c987
aob123 <alexobeswick@gmail.com>
2025-02-03 22:36:46 +0100

    removed base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/individuell_examination_frontend/"
+  // base: "/individuell_examination_frontend/"
 
 })
